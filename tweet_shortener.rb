4e04f6c4f4b042d5354517412d373b3f45a3a842
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 23:00:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,7 +28,7 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortner(tweet)
+def selective_tweet_shortener(tweet)
   if tweet.char.length > 140
   word_substituter(tweet)
 else
