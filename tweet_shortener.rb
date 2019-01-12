3cc03d49b41b932033c8c38744996fde19b21db9
Luis Zabala <luis.zabala076@gmail.com>
2019-01-12 23:07:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,7 @@ def shortened_tweet_truncator(tweets)
   if word_substituter(tweet).chars.length > 140
     truncated = word_substituter(tweet).chars[0..136].push("...").join("")
   else
-    tweet
+    word_substituter(tweet)
   end
 end
     
